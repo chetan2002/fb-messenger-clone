9202a33acd8ce4a7ad3f70fdd93bf7d9c034617c
chetan2002 <46553269+chetan2002@users.noreply.github.com>
2020-10-30 15:36:50 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React , { useState } from 'react';
+import { Button , FormControl , InputLabel , Input} from '@material-ui/core';
 import './App.css';
 
 function App() {
   const [input  , setInput] = useState('');
+  const [messages, setMessages] = useState(['hello' , 'Hi' , 'What']);
+
   console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    //all the logic to send a message goes
+    setMessages([...messages , input]);
+    setInput('');
+
+  }
   return (
     <div className="App">
       <h1> Hello Chetan!</h1>
+      <form>
+          <FormControl>
+            <InputLabel >Enter a message...</InputLabel>
+            <Input value = {input} onChange={event => setInput(event.target.value)}/>
+          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
 
-      <input value = {input} onChange={event => setInput(event.target.value)}/>
-      <button>Send Message</button>
-      {/* input field */}
-      {/* button */}
+          </FormControl>
+        </form>
+      
       {/* messages themselves */}
-
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
